663265604d2138a814248e8735cbd4c4c86c5203
gabibamboi-x <gabi.bamboi_x@icloud.com>
2022-10-14 23:04:49 +0200

    Add minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,27 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [ 'file-loader' ],
       }
     ]
-  }
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 9000,
+  },
 };
